c836c66a8695a47d80d83947e17a46791bca656f
Gregory Mitrano <gregory.mitrano@gmail.com>
2020-07-21 02:48:48 -0400

    Initial Work on Instruction Decode Logic
    This change adds some initial logic for decoding CPU instructions. It
    can currently decode instruction format type and all fields from any of
    those types. Some of the logic currently uses registers though and
    should probably be done with wires instead so it can be accessed during
    the decode stage.
    
    This change also removes the hardcoded halting logic in favor of logic
    that halts the CPU when an invalid instruction is encountered. This
    currently correctly stops the CPU once it runs off the end of the
    instruction buffer so it works for now...

--- hw/cpu.sv
+++ hw/cpu.sv
@@ -17,6 +17,8 @@ module cpu
 logic [31:0] r_pc;
 logic [31:0] r_inst_buf;
 
+reg [31:0] r_regs[30:0];
+
 typedef enum
 {
     cpu_state_init,
@@ -29,6 +31,49 @@ typedef enum
 
 cpu_state r_state;
 
+typedef enum
+{
+    inst_fmt_type_invalid,
+    inst_fmt_type_r,
+    inst_fmt_type_i,
+    inst_fmt_type_s,
+    inst_fmt_type_b,
+    inst_fmt_type_u,
+    inst_fmt_type_j
+} inst_fmt_type;
+
+// TODO: This is a test register
+inst_fmt_type r_inst_fmt_type;
+
+wire [4:0] w_inst_rd;
+assign w_inst_rd = r_inst_buf[11:7];
+
+wire [4:0] w_inst_rs1;
+assign w_inst_rs1 = r_inst_buf[19:15];
+
+wire [4:0] w_inst_rs2;
+assign w_inst_rs2 = r_inst_buf[24:20];
+
+wire [9:0] w_inst_func;
+assign w_inst_func = { r_inst_buf[31:25], r_inst_buf[14:12] };
+
+wire [11:0] w_inst_i_imm;
+assign w_inst_i_imm = { r_inst_buf[31:20] };
+
+wire [11:0] w_inst_s_imm;
+assign w_inst_s_imm = { r_inst_buf[31:25], r_inst_buf[11:7] };
+
+wire [11:0] w_inst_b_imm;
+assign w_inst_b_imm = { r_inst_buf[31], r_inst_buf[7], r_inst_buf[30:25], r_inst_buf[11:8] };
+
+wire [19:0] w_inst_u_imm;
+assign w_inst_u_imm = { r_inst_buf[31:12] };
+
+wire [19:0] w_inst_j_imm;
+assign w_inst_j_imm = { r_inst_buf[31], r_inst_buf[19:12], r_inst_buf[20], r_inst_buf[30:21] };
+
+reg [31:0] r_inst_imm;
+
 assign o_is_halted = (r_state == cpu_state_halt);
 
 always_ff @ (posedge i_clk)
@@ -40,6 +85,10 @@ always_ff @ (posedge i_clk)
 
             o_mem_write_en <= 0;
             o_mem_addr <= 0;
+
+            // TODO: These probably shouldn't be registers?
+            r_inst_fmt_type <= inst_fmt_type_invalid;
+            r_inst_imm <= 0;
         end
     else if (i_enable)
         begin
@@ -68,17 +117,44 @@ always_ff @ (posedge i_clk)
                     begin
                         r_inst_buf <= i_mem_data;
 
+                        // Determine the instruction format type so we can decode instruction fields
+                        case (i_mem_data[6:0])
+                            'b0110111,
+                            'b0010111: r_inst_fmt_type <= inst_fmt_type_u;
+                            'b1101111: r_inst_fmt_type <= inst_fmt_type_j;
+                            'b1100011: r_inst_fmt_type <= inst_fmt_type_b;
+                            'b0100011: r_inst_fmt_type <= inst_fmt_type_s;
+                            'b0110011: r_inst_fmt_type <= inst_fmt_type_r;
+                            'b1100111,
+                            'b0000011,
+                            'b0010011,
+                            'b0001111,
+                            'b1110011: r_inst_fmt_type <= inst_fmt_type_i;
+                            default: r_inst_fmt_type <= inst_fmt_type_invalid;
+                        endcase
+
                         r_state <= cpu_state_execute;
                     end
                 cpu_state_execute:
                     begin
-                        // TODO: Actually halt on real stuff
-                        if (r_pc <= 820)
+                        // Execute the instruction if it's valid
+                        if (r_inst_fmt_type != inst_fmt_type_invalid)
                             begin
+                                // TODO: This should definitely be calculated during the decode stage
+                                case (r_inst_fmt_type)
+                                    inst_fmt_type_i: r_inst_imm <= { { 20 { w_inst_i_imm[11] } }, w_inst_i_imm[11:0] };
+                                    inst_fmt_type_s: r_inst_imm <= { { 20 { w_inst_s_imm[11] } }, w_inst_s_imm[11:0] };
+                                    inst_fmt_type_b: r_inst_imm <= { { 19 { w_inst_b_imm[11] } }, w_inst_b_imm[11:0], 1'b0 };
+                                    inst_fmt_type_u: r_inst_imm <= { { 12 { w_inst_u_imm[19] } }, w_inst_u_imm[19:0] };
+                                    inst_fmt_type_j: r_inst_imm <= { { 11 { w_inst_j_imm[19] } }, w_inst_j_imm[19:0], 1'b0 };
+                                    default:         r_inst_imm <= 0;
+                                endcase
+
                                 r_state <= cpu_state_fetch;
                             end
                         else
                             begin
+                                // Move to the halted state if we encounter an invalid instruction
                                 r_state <= cpu_state_halt;
                             end
                     end
