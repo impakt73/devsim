01817f8ebb7b4e8cb92eb9d35357c5f52e6154a9
Gregory Mitrano <gregory.mitrano@gmail.com>
2020-07-27 01:33:53 -0400

    Disable VCD Traces in Release Builds
    This provides a small boost in performance and traces probably won't be
    needed in release builds in most cases.

--- build.rs
+++ build.rs
@@ -4,10 +4,17 @@ use std::path::Path;
 
 fn main() {
     let hw_dir_path = env::current_dir().unwrap().join(Path::new("hw"));
-    let dst = Config::new("third_party/protobridge")
-        .define("PROTOBRIDGE_HW_PATH", hw_dir_path)
-        .define("PROTOBRIDGE_TRACE", "ON")
-        .build();
+    let mut config = Config::new("third_party/protobridge");
+
+    config.define("PROTOBRIDGE_HW_PATH", hw_dir_path);
+
+    // Only enable VCD traces in debug builds
+    if cfg!(debug_assertions) {
+        config.define("PROTOBRIDGE_TRACE", "ON");
+    }
+
+    let dst = config.build();
+
     println!(
         "cargo:rustc-link-search=native={}",
         format!("{}/lib", dst.display())
